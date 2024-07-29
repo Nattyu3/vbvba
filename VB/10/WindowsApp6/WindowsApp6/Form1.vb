35a72f28945215a92479911fb596bda8e8be627c
Nattyu3 <72292434+Nattyu3@users.noreply.github.com>
2024-07-29 13:56:16 +0900

    Add files via upload

---
+++ VB/10/WindowsApp6/WindowsApp6/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+	Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+		MessageBox.Show("［OK］ボタン または [Enter] が押された", "結果")
+	End Sub
+
+	Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+		MessageBox.Show("［Cancel］ボタン または [Esc] が押された", "結果")
+		Me.Close()
+	End Sub
+End Class
