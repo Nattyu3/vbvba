7394ef7bf405e9bbdc38ad7c3abe0535d04fc663
Nattyu3 <72292434+Nattyu3@users.noreply.github.com>
2024-07-29 13:50:03 +0900

    Add files via upload

---
+++ VB/15/WindowsApp1/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,276 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuItemFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuItemFileNew = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuItemFileOpen = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuItemFileSave = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuItemFileSaveAs = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuItemFileSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.MenuItemFilePrintPreview = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuItemFilePrint = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuItemFileSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.MenuItemFileExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuItemEdit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuItemEditUndo = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuItemEditSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.MenuItemEditCut = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuItemEditCopy = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuItemEditPaste = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuItemEditDelete = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuItemEditSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.MenuItemEditSelectAll = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuItemSetting = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuItemSettingFont = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TextBoxMain = New System.Windows.Forms.TextBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.FontDialog1 = New System.Windows.Forms.FontDialog()
+        Me.PrintDialog1 = New System.Windows.Forms.PrintDialog()
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.PrintPreviewDialog1 = New System.Windows.Forms.PrintPreviewDialog()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuItemFile, Me.MenuItemEdit, Me.MenuItemSetting})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 28)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenuItemFile
+        '
+        Me.MenuItemFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuItemFileNew, Me.MenuItemFileOpen, Me.MenuItemFileSave, Me.MenuItemFileSaveAs, Me.MenuItemFileSeparator1, Me.MenuItemFilePrintPreview, Me.MenuItemFilePrint, Me.MenuItemFileSeparator2, Me.MenuItemFileExit})
+        Me.MenuItemFile.Name = "MenuItemFile"
+        Me.MenuItemFile.Size = New System.Drawing.Size(82, 24)
+        Me.MenuItemFile.Text = "ファイル(&F)"
+        '
+        'MenuItemFileNew
+        '
+        Me.MenuItemFileNew.Name = "MenuItemFileNew"
+        Me.MenuItemFileNew.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.N), System.Windows.Forms.Keys)
+        Me.MenuItemFileNew.Size = New System.Drawing.Size(231, 26)
+        Me.MenuItemFileNew.Text = "新規(&N)"
+        '
+        'MenuItemFileOpen
+        '
+        Me.MenuItemFileOpen.Name = "MenuItemFileOpen"
+        Me.MenuItemFileOpen.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.O), System.Windows.Forms.Keys)
+        Me.MenuItemFileOpen.Size = New System.Drawing.Size(231, 26)
+        Me.MenuItemFileOpen.Text = "開く(&O)"
+        '
+        'MenuItemFileSave
+        '
+        Me.MenuItemFileSave.Name = "MenuItemFileSave"
+        Me.MenuItemFileSave.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
+        Me.MenuItemFileSave.Size = New System.Drawing.Size(231, 26)
+        Me.MenuItemFileSave.Text = "上書き保存(&S)"
+        '
+        'MenuItemFileSaveAs
+        '
+        Me.MenuItemFileSaveAs.Name = "MenuItemFileSaveAs"
+        Me.MenuItemFileSaveAs.Size = New System.Drawing.Size(231, 26)
+        Me.MenuItemFileSaveAs.Text = "名前を付けて保存(&A)"
+        '
+        'MenuItemFileSeparator1
+        '
+        Me.MenuItemFileSeparator1.Name = "MenuItemFileSeparator1"
+        Me.MenuItemFileSeparator1.Size = New System.Drawing.Size(228, 6)
+        '
+        'MenuItemFilePrintPreview
+        '
+        Me.MenuItemFilePrintPreview.Name = "MenuItemFilePrintPreview"
+        Me.MenuItemFilePrintPreview.Size = New System.Drawing.Size(231, 26)
+        Me.MenuItemFilePrintPreview.Text = "印刷プレビュー(&V)"
+        '
+        'MenuItemFilePrint
+        '
+        Me.MenuItemFilePrint.Name = "MenuItemFilePrint"
+        Me.MenuItemFilePrint.Size = New System.Drawing.Size(231, 26)
+        Me.MenuItemFilePrint.Text = "印刷(&P)"
+        '
+        'MenuItemFileSeparator2
+        '
+        Me.MenuItemFileSeparator2.Name = "MenuItemFileSeparator2"
+        Me.MenuItemFileSeparator2.Size = New System.Drawing.Size(228, 6)
+        '
+        'MenuItemFileExit
+        '
+        Me.MenuItemFileExit.Name = "MenuItemFileExit"
+        Me.MenuItemFileExit.Size = New System.Drawing.Size(231, 26)
+        Me.MenuItemFileExit.Text = "終了(&X)"
+        '
+        'MenuItemEdit
+        '
+        Me.MenuItemEdit.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuItemEditUndo, Me.MenuItemEditSeparator1, Me.MenuItemEditCut, Me.MenuItemEditCopy, Me.MenuItemEditPaste, Me.MenuItemEditDelete, Me.MenuItemEditSeparator2, Me.MenuItemEditSelectAll})
+        Me.MenuItemEdit.Name = "MenuItemEdit"
+        Me.MenuItemEdit.Size = New System.Drawing.Size(71, 24)
+        Me.MenuItemEdit.Text = "編集(&E)"
+        '
+        'MenuItemEditUndo
+        '
+        Me.MenuItemEditUndo.Name = "MenuItemEditUndo"
+        Me.MenuItemEditUndo.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Z), System.Windows.Forms.Keys)
+        Me.MenuItemEditUndo.Size = New System.Drawing.Size(229, 26)
+        Me.MenuItemEditUndo.Text = "元に戻す(&U)"
+        '
+        'MenuItemEditSeparator1
+        '
+        Me.MenuItemEditSeparator1.Name = "MenuItemEditSeparator1"
+        Me.MenuItemEditSeparator1.Size = New System.Drawing.Size(226, 6)
+        '
+        'MenuItemEditCut
+        '
+        Me.MenuItemEditCut.Name = "MenuItemEditCut"
+        Me.MenuItemEditCut.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.X), System.Windows.Forms.Keys)
+        Me.MenuItemEditCut.Size = New System.Drawing.Size(229, 26)
+        Me.MenuItemEditCut.Text = "切り取り(&X)"
+        '
+        'MenuItemEditCopy
+        '
+        Me.MenuItemEditCopy.Name = "MenuItemEditCopy"
+        Me.MenuItemEditCopy.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.C), System.Windows.Forms.Keys)
+        Me.MenuItemEditCopy.Size = New System.Drawing.Size(229, 26)
+        Me.MenuItemEditCopy.Text = "コピー(&C)"
+        '
+        'MenuItemEditPaste
+        '
+        Me.MenuItemEditPaste.Name = "MenuItemEditPaste"
+        Me.MenuItemEditPaste.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.P), System.Windows.Forms.Keys)
+        Me.MenuItemEditPaste.Size = New System.Drawing.Size(229, 26)
+        Me.MenuItemEditPaste.Text = "貼り付け(&P)"
+        '
+        'MenuItemEditDelete
+        '
+        Me.MenuItemEditDelete.Name = "MenuItemEditDelete"
+        Me.MenuItemEditDelete.ShortcutKeys = System.Windows.Forms.Keys.Delete
+        Me.MenuItemEditDelete.Size = New System.Drawing.Size(229, 26)
+        Me.MenuItemEditDelete.Text = "削除(&L)"
+        '
+        'MenuItemEditSeparator2
+        '
+        Me.MenuItemEditSeparator2.Name = "MenuItemEditSeparator2"
+        Me.MenuItemEditSeparator2.Size = New System.Drawing.Size(226, 6)
+        '
+        'MenuItemEditSelectAll
+        '
+        Me.MenuItemEditSelectAll.Name = "MenuItemEditSelectAll"
+        Me.MenuItemEditSelectAll.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.A), System.Windows.Forms.Keys)
+        Me.MenuItemEditSelectAll.Size = New System.Drawing.Size(229, 26)
+        Me.MenuItemEditSelectAll.Text = "すべて選択(&A)"
+        '
+        'MenuItemSetting
+        '
+        Me.MenuItemSetting.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuItemSettingFont})
+        Me.MenuItemSetting.Name = "MenuItemSetting"
+        Me.MenuItemSetting.Size = New System.Drawing.Size(74, 24)
+        Me.MenuItemSetting.Text = "書式(&O)"
+        '
+        'MenuItemSettingFont
+        '
+        Me.MenuItemSettingFont.Name = "MenuItemSettingFont"
+        Me.MenuItemSettingFont.Size = New System.Drawing.Size(224, 26)
+        Me.MenuItemSettingFont.Text = "フォント(&F)"
+        '
+        'TextBoxMain
+        '
+        Me.TextBoxMain.Location = New System.Drawing.Point(340, 261)
+        Me.TextBoxMain.Name = "TextBoxMain"
+        Me.TextBoxMain.Size = New System.Drawing.Size(100, 22)
+        Me.TextBoxMain.TabIndex = 1
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'PrintDialog1
+        '
+        Me.PrintDialog1.UseEXDialog = True
+        '
+        'PrintDocument1
+        '
+        '
+        'PrintPreviewDialog1
+        '
+        Me.PrintPreviewDialog1.AutoScrollMargin = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.AutoScrollMinSize = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.ClientSize = New System.Drawing.Size(400, 300)
+        Me.PrintPreviewDialog1.Enabled = True
+        Me.PrintPreviewDialog1.Icon = CType(resources.GetObject("PrintPreviewDialog1.Icon"), System.Drawing.Icon)
+        Me.PrintPreviewDialog1.Name = "PrintPreviewDialog1"
+        Me.PrintPreviewDialog1.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBoxMain)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MenuItemFile As ToolStripMenuItem
+    Friend WithEvents MenuItemFileExit As ToolStripMenuItem
+    Friend WithEvents TextBoxMain As TextBox
+    Friend WithEvents MenuItemFileNew As ToolStripMenuItem
+    Friend WithEvents MenuItemFileOpen As ToolStripMenuItem
+    Friend WithEvents MenuItemFileSave As ToolStripMenuItem
+    Friend WithEvents MenuItemFileSaveAs As ToolStripMenuItem
+    Friend WithEvents MenuItemFileSeparator1 As ToolStripSeparator
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents MenuItemEdit As ToolStripMenuItem
+    Friend WithEvents MenuItemEditUndo As ToolStripMenuItem
+    Friend WithEvents MenuItemEditSeparator1 As ToolStripSeparator
+    Friend WithEvents MenuItemEditCut As ToolStripMenuItem
+    Friend WithEvents MenuItemEditCopy As ToolStripMenuItem
+    Friend WithEvents MenuItemEditPaste As ToolStripMenuItem
+    Friend WithEvents MenuItemEditDelete As ToolStripMenuItem
+    Friend WithEvents MenuItemEditSeparator2 As ToolStripSeparator
+    Friend WithEvents MenuItemEditSelectAll As ToolStripMenuItem
+    Friend WithEvents MenuItemSetting As ToolStripMenuItem
+    Friend WithEvents MenuItemSettingFont As ToolStripMenuItem
+    Friend WithEvents FontDialog1 As FontDialog
+    Friend WithEvents MenuItemFilePrintPreview As ToolStripMenuItem
+    Friend WithEvents MenuItemFilePrint As ToolStripMenuItem
+    Friend WithEvents MenuItemFileSeparator2 As ToolStripSeparator
+    Friend WithEvents PrintDialog1 As PrintDialog
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+    Friend WithEvents PrintPreviewDialog1 As PrintPreviewDialog
+End Class
---
+++ VB/15/WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,160 @@
+﻿Public Class Form1
+	Private FileName As String = ""  '　名前を付けて保存対策
+	Private PrintString As String
+
+	Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+		Me.Text = "MyMemo"
+		TextBoxMain.Multiline = True
+		TextBoxMain.ScrollBars = ScrollBars.Vertical
+		TextBoxMain.Dock = DockStyle.Fill
+		SaveFileDialog1.Filter = "テキスト文章|*.txt|すべてのファイル|*.**"
+		MenuItemEdit_DropDownOpening(sender, e)
+		FontDialog1.ShowEffects = False
+		FontDialog1.AllowScriptChange = False
+
+		PrintDialog1.Document = PrintDocument1
+		PrintPreviewDialog1.Document = PrintDocument1
+	End Sub
+
+	'　「終了」メソッド
+	Private Sub MenuItemFileExit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MenuItemFileExit.Click
+		Me.Close()
+	End Sub
+
+	'　「新規」メソッド
+	Private Sub MenuItemFileNew_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MenuItemFileNew.Click
+		TextBoxMain.Text = “”
+	End Sub
+
+	'　「開く」メソッド
+	Private Sub MenuItemFileOpen_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MenuItemFileOpen.Click
+		OpenFileDialog1.FileName = ""
+		'OpenFileDialog1.ShowDialog()
+		'LoadFile(OpenFileDialog1.FileName)
+		If DialogResult.OK = OpenFileDialog1.ShowDialog() Then
+			LoadFile(OpenFileDialog1.FileName)
+		End If
+		SaveFileDialog1.FileName = OpenFileDialog1.FileName
+	End Sub
+
+	'　LoadFileメソッド
+	Private Sub LoadFile(ByVal Value As String)
+		TextBoxMain.Text = System.IO.File.ReadAllText(Value)
+		FileName = Value  '　（追加）名前を付けて保存対策
+	End Sub
+
+	'　「上書き保存」メソッド
+	Private Sub MenuItemFileSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MenuItemFileSave.Click
+		SaveFile(SaveFileDialog1.FileName)
+	End Sub
+
+	'　「名前を付けて保存」メソッド
+	Private Sub MenuItemFileSaveAs_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MenuItemFileSaveAs.Click
+		'　（追加）名前を付けて保存対策
+		SaveFileDialog1.FileName = System.IO.Path.GetFileName(FileName)
+		If DialogResult.OK = SaveFileDialog1.ShowDialog() Then
+			SaveFile(SaveFileDialog1.FileName)
+		End If
+	End Sub
+
+	'　SaveFileメソッド
+	Private Sub SaveFile(ByVal Value As String)
+		System.IO.File.WriteAllText(Value, TextBoxMain.Text)
+		FileName = Value  '　（追加）名前を付けて保存対策
+	End Sub
+
+	'　「切り取り」メソッド
+	Private Sub MenuItemEditCut_Click(sender As Object, e As EventArgs) Handles MenuItemEditCut.Click
+		TextBoxMain.Cut()
+	End Sub
+
+	'　「コピー」メソッド
+	Private Sub MenuItemEditCopy_Click(sender As Object, e As EventArgs) Handles MenuItemEditCopy.Click
+		TextBoxMain.Copy()
+	End Sub
+
+	'　「貼り付け」メソッド
+	Private Sub MenuItemEditPaste_Click(sender As Object, e As EventArgs) Handles MenuItemEditPaste.Click
+		TextBoxMain.Paste()
+	End Sub
+
+	'　「削除」メソッド
+	Private Sub MenuItemEditDelete_Click(sender As Object, e As EventArgs) Handles MenuItemEditDelete.Click
+		TextBoxMain.SelectedText = ""
+	End Sub
+
+	'　「すべて選択」メソッド
+	Private Sub MenuItemEditSelectAll_Click(sender As Object, e As EventArgs) Handles MenuItemEditSelectAll.Click
+		TextBoxMain.SelectAll()
+	End Sub
+
+	' Deleteキー（1文字を削除）対策　（飛ばしても良い）
+	Private Sub MenuItemEdit_DropDownOpening(sender As Object, e As EventArgs) Handles MenuItemEdit.DropDownOpening
+		MenuItemEditPaste.Enabled = Clipboard.ContainsText
+		Dim sa = TextBoxMain.SelectionLength = 0
+		MenuItemEditCut.Enabled = Not sa
+		MenuItemEditCopy.Enabled = Not sa
+		MenuItemEditDelete.Enabled = Not sa
+		sa = TextBoxMain.TextLength = 0
+		MenuItemEditSelectAll.Enabled = Not sa
+	End Sub
+
+	' Deleteキー（1文字を削除）対策
+	Private Sub MenuItemEdit_DropDownClosed(sender As Object, e As EventArgs) Handles MenuItemEdit.DropDownClosed
+		MenuItemEditDelete.Enabled = False
+	End Sub
+
+	' 「フォント」メソッド
+	Private Sub MenuItemSettingFont_Click(sender As Object, e As EventArgs) Handles MenuItemSettingFont.Click
+		FontDialog1.Font = TextBoxMain.Font
+		If DialogResult.OK = FontDialog1.ShowDialog() Then
+			TextBoxMain.Font = FontDialog1.Font
+		End If
+	End Sub
+
+	' 「印刷」メソッド
+	Private Sub MenuItemFilePrint_Click(sender As Object, e As EventArgs) Handles MenuItemFilePrint.Click
+		'SetPrintDocument1()		‘コメント文に変更
+		'PrintDocument1.Print()
+
+		If DialogResult.OK = PrintDialog1.ShowDialog() Then 'Dialog付
+			SetPrintDocument1()
+			PrintDocument1.Print()
+		End If
+	End Sub
+
+	'　SetPrintDocument1メソッド
+	Private Sub SetPrintDocument1()
+		PrintString = TextBoxMain.Text
+		PrintDocument1.DefaultPageSettings.Margins = New Printing.Margins(20, 60, 20, 60)
+		PrintDocument1.DocumentName = FileName
+	End Sub
+
+	'　PrintDocument1_PrintPageメソッド
+	Private Sub PrintDocument1_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles PrintDocument1.PrintPage
+		Dim charactersOnPage As Integer = 0
+		Dim linesPerPage As Integer = 0
+		e.Graphics.MeasureString(PrintString, TextBoxMain.Font, e.MarginBounds.Size, StringFormat.GenericTypographic, charactersOnPage, linesPerPage)
+		e.Graphics.DrawString(PrintString, TextBoxMain.Font, Brushes.Black, e.MarginBounds, StringFormat.GenericTypographic)
+		PrintString = PrintString.Substring(charactersOnPage)
+		If PrintString.Length > 0 Then
+			e.HasMorePages = True
+		Else
+			e.HasMorePages = False
+			PrintString = TextBoxMain.Text
+		End If
+	End Sub
+
+	'　「印刷プレビュー」メソッド
+	Private Sub MenuItemFilePrintPreview_Click(sender As Object, e As EventArgs) Handles MenuItemFilePrintPreview.Click
+
+		Dim b = TextBoxMain.TextLength = 0
+		MenuItemFilePrint.Enabled = Not b
+		MenuItemFilePrintPreview.Enabled = Not b
+
+		SetPrintDocument1()
+		PrintPreviewDialog1.ShowDialog()
+
+	End Sub
+
+End Class
