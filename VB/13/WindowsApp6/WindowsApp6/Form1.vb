f5b9166fe4497cb237074f47f2d16f5061030626
Nattyu3 <72292434+Nattyu3@users.noreply.github.com>
2024-07-29 14:03:17 +0900

    Add files via upload

---
+++ VB/13/WindowsApp6/WindowsApp6/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Structure myMenu      '構造体を定義
+        Public Menu As String
+        Public Price As Integer
+    End Structure
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim kzt As myMenu
+        '構造体の値を設定し、プロシージャを呼び出す
+        kzt.Menu = "スーツ"
+        kzt.Price = 54800
+        ShowData(kzt)
+    End Sub
+
+    '構造体を引数として受け取るプロシージャ
+    Private Sub ShowData(ByVal nMenu As myMenu)
+        MessageBox.Show(nMenu.Menu & "：" & nMenu.Price.ToString & "円", "●●デパート")
+    End Sub
+End Class
---
+++ VB/13/WindowsApp7/WindowsApp7/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Structure myPoint      '構造体を定義
+        Public xPoint As Integer
+        Public yPoint As Integer
+    End Structure
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim pt As myPoint
+        'プロシージャを呼び出して、構造体を戻り値として受け取る
+        pt = TwicePoint(10, 25)
+        MessageBox.Show("　xの位置　：　" & pt.xPoint & vbCrLf _
+                  & "　yの位置　：　" & pt.yPoint, "戻り値")
+    End Sub
+
+    '構造体を戻り値として返すプロシージャ
+    Private Function TwicePoint(ByVal x As Integer, ByVal y As Integer) As myPoint
+        Dim DispPoint As myPoint        '構造体型変数を宣言
+        DispPoint.xPoint = x * 5
+        DispPoint.yPoint = y * 5
+        Return DispPoint                '構造体を返す
+    End Function
+End Class
+
