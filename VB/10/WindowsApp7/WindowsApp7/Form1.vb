0662976aee770017d0f42e034fdbe93628ad2a67
Nattyu3 <72292434+Nattyu3@users.noreply.github.com>
2024-07-29 13:57:46 +0900

    Add files via upload

---
+++ VB/10/WindowsApp7/WindowsApp7/Form1.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Reflection.Emit
+Public Class Form1
+	Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+		'Me.Button1.Text = "OK"
+		'Me.Button2.Text = "Cancel"
+		Me.AcceptButton = Button1
+		Me.CancelButton = Button2
+		Label1.Text = ("フォーム読み込み時に承認ボタン、キャンセルボタンを設定しました")
+	End Sub
+	Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+		MessageBox.Show("［OK］ボタンがクリックされました")
+	End Sub
+	Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+		MessageBox.Show("キャンセルします")
+		Me.Close()
+	End Sub
+End Class
---
+++ VB/10/WindowsApp8/WindowsApp8/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        Label1.Text = "透明度   " & CInt(Me.Opacity * 100) & "%"
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.Opacity -= 0.1
+        Label1.Text = "透明度   " & CInt(Me.Opacity * 100) & "%"
+    End Sub
+End Class
